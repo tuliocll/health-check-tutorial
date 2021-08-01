eae74dc55ec8be7a65c7d5543b9ceccf8670a822
Tulio <tuliocll@gmail.com>
2021-08-01 01:14:16 -0300

    :bug: fix heroku port

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const defaultRouter = require("./routes/default");
 const heathChecktRouter = require("./routes/healthCheck");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(defaultRouter);
 
